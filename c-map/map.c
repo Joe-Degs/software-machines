4e60e33f3ad216a834c25f6504113225352cff6e
Joe-Degs <jpedegs8990@gmail.com>
2021-06-01 16:31:27 +0100

    hash map implementation
    learnt most the things about hash map through this implementation.

--- c-map/map.c
+++ c-map/map.c
@@ -12,7 +12,7 @@ typedef struct map map_t;
 
 map_t *map_new(int size) <{
     map_t *m = malloc(sizeof(map_t));
-    m->chains = malloc(sizeof(assoc_array_t) * size);
+    m->chains = malloc(sizeof(assoc_array_t*) * size);
     for (int i = 0; i < size; i++) {
         m->chains[i] = NULL
     }
@@ -41,7 +41,19 @@ void map_set(map_t *m, char *k, char *v) {
     int b = map_chain(m, k);
     assoc_array_t *a = m->chains[b];
     search_t *s = search_find(a, k);
-    if (s->value != NULL) {
+    if (s->value != NULL) { /* change the value if key already is present */
         s->cursor->value = strdup(v);
+    } else { /* we do not have that (key, value) in our map */
+        assoc_array_t  *n = assoc_array_new(k, v);
+        if (s->cursor == a) {
+            n->next = s->cursor;
+            m->chains[b] = n;
+        } else if (s->cursor == NULL) {
+            s->memo->next = n;
+        } else {
+            n->next = s->cursor;
+            s->memo->next = n;
+        }
     }
+    free(s);
 }
